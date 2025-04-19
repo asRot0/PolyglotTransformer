267e7218121afb5b6194cf2638b5d34b1dd6a981
asif <asahmed8890@gmail.com>
2025-04-19 19:25:36 +0700

    Add Transformer Encoder implementation

--- models/transformer_encoder.py
+++ models/transformer_encoder.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
+from tensorflow.keras import layers
 from tensorflow.keras.layers import LayerNormalization, Dropout
 from .multi_head_attention import MultiHeadSelfAttention
 from .feed_forward import FeedForwardNetwork
 
+
 class TransformerEncoderBlock(tf.keras.layers.Layer):
-    def __init__(self, embed_size, heads, ff_expansion=4, dropout_rate=0.1):
-        super().__init__()
-        self.attention = MultiHeadSelfAttention(embed_size, heads)
+    def __init__(self, embed_size, heads, ff_expansion=4, dropout_rate=0.1, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_size = embed_size
+        self.heads = heads
+        self.dense_dim = embed_size * ff_expansion
+        # self.attention = MultiHeadSelfAttention(embed_size, heads)
+        self.attention = layers.MultiHeadAttention(num_heads=heads, key_dim=embed_size)
         self.norm1 = LayerNormalization(epsilon=1e-6)
         self.norm2 = LayerNormalization(epsilon=1e-6)
         self.ffn = FeedForwardNetwork(embed_size, ff_expansion, dropout_rate)
@@ -14,8 +20,11 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
         self.dropout2 = Dropout(dropout_rate)
 
     def call(self, x, mask=None, training=False):
+        if mask is not None:
+            mask = mask[:, tf.newaxis, :]
+
         # Self-attention layer + Add & Norm
-        attention_output = self.attention(x, x, x, mask)
+        attention_output = self.attention(x, x, attention_mask=mask)
         attention_output = self.dropout1(attention_output, training=training)
         out1 = self.norm1(x + attention_output)
 
@@ -25,3 +34,12 @@ class TransformerEncoderBlock(tf.keras.layers.Layer):
         out2 = self.norm2(out1 + ffn_output)
 
         return out2
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "embed_dim": self.embed_size,
+            "num_heads": self.heads,
+            "dense_dim": self.dense_dim,
+        })
+        return config
