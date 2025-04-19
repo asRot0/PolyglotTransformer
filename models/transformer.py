7140c025662ec41512dbc1dae9fcd1d7fc0559a2
asif <asahmed8890@gmail.com>
2025-04-19 19:27:08 +0700

    Add full Transformer architecture combining encoder and decoder

--- models/transformer.py
+++ models/transformer.py
@@ -3,6 +3,8 @@ from tensorflow.keras.layers import Embedding, Dropout, Dense
 from .transformer_encoder import TransformerEncoderBlock
 from .transformer_decoder import TransformerDecoderBlock
 from .positional_encoding import PositionalEncoding
+from .positional_embedding import PositionalEmbedding
+
 
 class Transformer(tf.keras.Model):
     def __init__(self,
@@ -16,10 +18,13 @@ class Transformer(tf.keras.Model):
                  dropout_rate=0.1):
         super().__init__()
 
-        self.token_embedding_input = Embedding(input_vocab_size, embed_size)
-        self.token_embedding_target = Embedding(target_vocab_size, embed_size)
-        self.pos_encoding_input = PositionalEncoding(max_seq_length, embed_size)
-        self.pos_encoding_target = PositionalEncoding(max_seq_length, embed_size)
+        # self.token_embedding_input = Embedding(input_vocab_size, embed_size)
+        # self.token_embedding_target = Embedding(target_vocab_size, embed_size)
+        # self.pos_encoding_input = PositionalEncoding(max_seq_length, embed_size)
+        # self.pos_encoding_target = PositionalEncoding(max_seq_length, embed_size)
+
+        self.pos_encoding_input = PositionalEmbedding(max_seq_length, input_vocab_size, embed_size)
+        self.pos_encoding_target = PositionalEmbedding(max_seq_length, input_vocab_size, embed_size)
 
         self.encoder_layers = [
             TransformerEncoderBlock(embed_size, num_heads, ff_expansion, dropout_rate)
@@ -34,14 +39,15 @@ class Transformer(tf.keras.Model):
         self.dropout_input = Dropout(dropout_rate)
         self.dropout_target = Dropout(dropout_rate)
 
-        self.final_linear = Dense(target_vocab_size)
+        self.final_linear = Dense(target_vocab_size, activation="softmax")
 
-    def call(self, data, enc_padding_mask=None, look_ahead_mask=None, dec_padding_mask=None, training=False):
-        inputs, targets = data
+    def call(self, inputs, enc_padding_mask=None, dec_padding_mask=None, training=False):
+        encoder_inputs = inputs["english"]
+        decoder_inputs = inputs["bengla"]
 
         # Embed and add positional encoding to encoder inputs
-        enc_embed = self.token_embedding_input(inputs)
-        enc_embed = self.pos_encoding_input(enc_embed)
+        # enc_embed = self.token_embedding_input(encoder_inputs)
+        enc_embed = self.pos_encoding_input(encoder_inputs)
         enc_embed = self.dropout_input(enc_embed, training=training)
 
         # Pass through encoder stack
@@ -49,13 +55,13 @@ class Transformer(tf.keras.Model):
             enc_embed = layer(enc_embed, mask=enc_padding_mask, training=training)
 
         # Embed and add positional encoding to decoder inputs
-        dec_embed = self.token_embedding_target(targets)
-        dec_embed = self.pos_encoding_target(dec_embed)
+        # dec_embed = self.token_embedding_target(decoder_inputs)
+        dec_embed = self.pos_encoding_target(decoder_inputs)
         dec_embed = self.dropout_target(dec_embed, training=training)
 
         # Pass through decoder stack
         for layer in self.decoder_layers:
-            dec_embed = layer(dec_embed, enc_embed, look_ahead_mask, dec_padding_mask, training=training)
+            dec_embed = layer(dec_embed, enc_embed, dec_padding_mask, training=training)
 
         # Final output layer: project decoder outputs to target vocab size
         final_output = self.final_linear(dec_embed)
